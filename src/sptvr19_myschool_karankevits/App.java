da2f5864ba47bba728e93f50b8d263793ce5e71c
Christofer Karankevits <chris.karankevits@gmail.com>
2020-10-21 10:28:24 +0300

    MySchool

---
+++ src/sptvr19_myschool_karankevits/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19_myschool_karankevits;
+
+/**
+ *
+ * @author user
+ */
+class App {
+    public void run(){
+        System.out.println("Моя Школа");
+    }
+}
---
+++ src/sptvr19_myschool_karankevits/SPTVR19_MySchool_Karankevits.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19_myschool_karankevits;
+
+/**
+ *
+ * @author user
+ */
+public class SPTVR19_MySchool_Karankevits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+    
+}
